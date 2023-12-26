2a104d041880a6d8c8f8a91ce0d67573d0c5a619
Sariot Hossain <sariothossain1011@gmail.com>
2023-12-26 19:20:52 +0600

    add sign-in & sign-up feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
 import { Poppins } from 'next/font/google'
 import './globals.css'
 
 const poppins = Poppins({
-   subsets: ['latin'],
-   weight:['400','500','600','700'],
-   variable:'--font-poppins'
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins'
 
-  
-  })
+
+})
 
 export const metadata: Metadata = {
   title: 'Evently',
@@ -21,8 +22,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,16 +1,24 @@
+import { SignedOut } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
 
 
 const Header = () => {
     return (
         <header className=" w-full border-b">
             <div className=" wrapper flex items-center justify-between">
-                <Link href='/' className=" w-36">
-                    <Image src='' width={128}  height={38} alt="loto image"/>
-                    Evently</Link>
-                <div className=" flex w-32 justify-end gap-3">Login</div>
-                 </div>
+                <Link href='' className=" w-36">
+                    <Image src='/assets/images/logo.png' width={128} height={38} alt="loto image" />
+                </Link>
+                <div className=" flex w-32 justify-end gap-3">
+                    <SignedOut>
+                        <Button asChild className=" rounded-full" size='lg'>
+                            <Link href='/sign-id'>Sign In</Link>
+                        </Button>
+                    </SignedOut>
+                </div>
+            </div>
         </header>
     )
 }
