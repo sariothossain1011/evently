f92a0e9765c6f75aeddb5313c7e49389ab41de1f
Sariot Hossain <sariothossain1011@gmail.com>
2023-12-27 16:02:25 +0600

    simple footer section complete

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,14 @@
-
+import Link from "next/link"
+import Image from "next/image"
 const Footer = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className=" border-t">
+            <div className="flex-center wrapper flex-between flex flex-col gap-5 p-5 text-center md:flex-row">
+                <Link href='/'>
+                <Image src='/assets/images/logo.png' width={128} height={38} alt="loto image" />
+                </Link>
+                <p>&copy; 2023 Evently. All Rights reserved by Sariot Hossain</p>
+            </div>
         </footer>
     )
 }
